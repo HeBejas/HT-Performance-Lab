4539f1c8af797fd6c014a0a3b756058cc4de3b8f
Bogdan <bogdan.aleksandrov.01@mail.ru>
2024-07-09 13:59:12 +0400

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -7,26 +7,26 @@ def factorial( n ):
             result += str(i)
         return result
 
-n = int(input())
-    
+n = int(input('Введите n: '))
+m = int(input('Введите m: '))    
 array = factorial(n)
 
+if n == m:
+    print('n не может быть равен m')
 if n < 2:
     print('n не может быть меньше 2')
 else:
-    if n == 2:
-        m = 2
-    else:
-        m = n - 1
-        
     answer = ''
     i = 0
     while(True):
-        answer += array[i:m+i]
+        step = array[i:m+i]
+        answer += step[0]
         i += m-1
         array += array
-        if answer[-1] == '1': 
+        if step[-1] == '1': 
             break
     print(answer)
+
+input()
 
 
