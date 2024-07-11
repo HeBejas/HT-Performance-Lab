7bf5e54941ce4e9ef3124974bd6f963619ff71b2
Bogdan <bogdan.aleksandrov.01@mail.ru>
2024-07-11 15:45:25 +0400

    create task4

---
+++ task4/task4.py
@@ -0,0 +1,14 @@
+import statistics
+
+filename = input('Введитье название файла - ')
+with open(filename, 'r') as file:
+    numbers = file.readlines()
+    numbers = [number.strip() for number in numbers]
+
+numbers = [int(num) for num in numbers]
+  
+average = round(statistics.mean(numbers))
+
+answer = sum( abs(average - number ) for number in numbers )
+
+print(answer)
